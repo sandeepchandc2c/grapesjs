88c33aa3d6aa90570a51ec28356f89243913a244
gaurav <sandeepchand>
2021-08-03 11:45:11 +0530

    make changes in code

--- client/src/view/Signature.js
+++ client/src/view/Signature.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import Popup from "reactjs-popup";
 import SignaturePad from "react-signature-canvas";
+import {Row, Col, Card, CardBody} from "reactstrap"
 import "./sigCanvas.css";
 import "../App.css"
 import axios from "axios";
@@ -9,7 +10,7 @@ function App() {
 
   const sigCanvas = useRef({});
   useEffect(()=>{
-    axios.get("http://localhost:3001/getsign").then(res=> console.log(res.data.signature))
+    axios.get("http://localhost:3001/getsign").then(res=> setImageURL(res.data.signature))
   }, [])
   /* a function that uses the canvas ref to clear the canvas 
   via a method given by react-signature-canvas */
@@ -26,45 +27,57 @@ function App() {
   }
 
   return (
-    <div >
-      <h1>Signature Pad Example</h1>
-      <Popup
-        modal
-        trigger={<button className="btn-btn-primary">Open Signature Pad</button>}
-        closeOnDocumentClick={false}
-      >
-        {close => (
-          <>
-            <SignaturePad
-              ref={sigCanvas}
-              canvasProps={{
-                className: "signatureCanvas"
-              }}
-            />
-            {/* Button to trigger save canvas image */}
-            <div className="btn btn-success" onClick={save}>Save</div>
-            <div className="btn btn-danger" onClick={clear}>Clear</div>
-            <div className="btn btn-danger" onClick={close}>Close</div>
-          </>
-        )}
-      </Popup>
-      <br />
-      <br />
-      {/* if our we have a non-null image url we should 
-      show an image and pass our imageURL state to it*/}
-      {imageURL ? (
-        <img
-          src={imageURL}
-          alt="my signature"
-          style={{
-            display: "block",
-            margin: "0 auto",
-            border: "1px solid black",
-            width: "150px"
-          }}
-        />
-      ) : null}
-    </div>
+    <Row className="justify-content-center">
+      <Col md={6}>
+       <Card>
+         <CardBody>
+          <Row className="justify-content-center">
+            <Col md={6}>
+          {imageURL ? (
+          <img
+            src={imageURL}
+            alt="my signature"
+            style={{
+              display: "block",
+              margin: "0 auto",
+              border: "1px solid black",
+              width: "150px"
+            }}
+          />
+        ) : null}
+          </Col>
+            </Row>
+            <Row className="justify-content-center py-4">
+              <Col md={6}>
+            <Popup
+              modal
+              trigger={<center><button className="btn btn-primary">Open Signature Pad</button></center>}
+              closeOnDocumentClick={false}
+            >
+              {close => (
+                <>
+                  <SignaturePad
+                    ref={sigCanvas}
+                    canvasProps={{
+                      className: "signatureCanvas"
+                    }}
+                  />
+                  {/* Button to trigger save canvas image */}
+                  <div className="btn btn-success" onClick={save}>Save</div>
+                  <div className="btn btn-danger" onClick={clear}>Clear</div>
+                  <div className="btn btn-danger" onClick={close}>Close</div>
+                </>
+              )}
+            </Popup>
+        
+          </Col>
+            </Row>
+         </CardBody>
+       </Card>
+   
+      </Col>
+    </Row>
+    
   );
 }
 
--- test.js
+++ test.js
@@ -4,6 +4,90 @@ const express = require("express")
 const app = express()
 
 app.use(express.static('testlib'))
+var html_to_pdf = require('html-pdf-node');
+const fs = require("fs")
+let options = { format: 'A4' };
+// Example of options with args //
+// let options = { format: 'A4', args: ['--no-sandbox', '--disable-setuid-sandbox'] };
+
+let file = { content: `<input type="text"></input>` };
+// or //
+// let file = { url: "https://example.com" };
+// html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
+//     fs.writeFileSync(__dirname+"/two.pdf",pdfBuffer, 
+//     (err)=>{
+//         console.log("err",err)
+//     } )
+//   console.log("PDF Buffer:-", pdfBuffer);
+// });
+
+
+//
+//2
+var pdf = require('html-pdf');
+// pdf.create(`<input type="text"></input>`, options).toFile(__dirname+'/businesscard.pdf', function(err, res) {
+//     if (err) return console.log(err);
+//     console.log(res); // { filename: '/app/businesscard.pdf' }
+//   });
+//3
+var htmlToPdf = require('html-to-pdf');
+// htmlToPdf.convertHTMLFile(`<input type="password"></input>`, 'next.pdf',
+//     function (error, success) {
+//        if (error) {
+//             console.log('Oh noes! Errorz!');
+//             console.log(error);
+//         } else {
+//             console.log('Woot! Success!');
+            
+//             console.log(success);
+//         }
+//     }
+// );
+
+// 4 test 
+// var conversion = require("phantom-html-to-pdf")();
+// conversion({ html: `<input type="password"></input>`}, function(err, pdf) {
+//     var output = fs.createWriteStream('output.pdf')
+//     console.log(pdf.logs);
+//     console.log(pdf.numberOfPages);
+//       // since pdf.stream is a node.js stream you can use it
+//       // to save the pdf to a file (like in this example) or to
+//       // respond an http request.
+//     pdf.stream.pipe(output);
+ 
+// });
+// 5
+const HTML5ToPDF = require("html5-to-pdf")
+const path = require("path")
+ 
+// const run = async () => {
+//   const html5ToPDF = new HTML5ToPDF({
+//     inputPath: path.join(__dirname, "kbc.html"),
+//     outputPath: path.join(__dirname, "output1.pdf"),
+//     // include: [
+//     //   path.join(__dirname, "assets", "basic.css"),
+//     //   path.join(__dirname, "assets", "custom-margin.css"),
+//     // ],
+//   })
+ 
+//   await html5ToPDF.start()
+//   await html5ToPDF.build()
+//   await html5ToPDF.close()
+// }
+ 
+// (async () => {
+//   try {
+//     await run()
+//     console.log("DONE")
+//   } catch (error) {
+//     console.error(error)
+//     process.exitCode = 1
+//   } finally {
+//     process.exit();
+//   }
+// })()
+ 
+ 
 app.listen(3001, ()=>{
     console.log("Server Started")
 })
